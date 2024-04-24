7a152acd88ac79f997cbb2603a94b9490d7c3072
lulu <lulukalmisfer@hotmail.com>
2024-04-24 10:36:22 +0300

    add new receiver service

---
+++ Receiver-Service/src/main/java/com/panic/receiverservice/ReceiverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.panic.receiverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReceiverServiceApplication.class, args);
+    }
+
+}

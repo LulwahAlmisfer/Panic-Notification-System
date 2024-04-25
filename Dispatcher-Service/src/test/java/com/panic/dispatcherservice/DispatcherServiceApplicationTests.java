068a7d9d740d90fd267aaf94b115826f666f3d2e
Lulwah Almisfer <67158449+LulwahAlmisfer@users.noreply.github.com>
2024-04-25 08:25:11 +0300

    add Dispatcher Service

---
+++ Dispatcher-Service/src/test/java/com/panic/dispatcherservice/DispatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panic.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

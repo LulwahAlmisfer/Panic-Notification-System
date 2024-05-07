2697a8796bf5696f522c01d11e444c3951e5cbf8
gyda13 <gyda2100@gmail.com>
2024-05-07 09:30:28 +0300

    feat: add API-Key header

--- Filter-Service/src/main/java/org/example/filterservice/client/PanicNotificationClient.java
+++ Filter-Service/src/main/java/org/example/filterservice/client/PanicNotificationClient.java
@@ -3,13 +3,13 @@ package org.example.filterservice.client;
 import lombok.extern.slf4j.Slf4j;
 import org.example.filterservice.client.models.Client;
 import org.example.filterservice.client.models.Notification;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
 
 import java.util.Collections;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
@@ -21,14 +21,19 @@ public class PanicNotificationClient {
 
     public static final String baseURL = "http://localhost:8080";
     public static final String clientURL = "/v1/client";
+    public static final String X_API_KEY = "X-API-KEY";
 
     RestClient restClient = RestClient.create();
+
+    @Value("${admin-key}")
+    private String adminKey;
 
     public Set<Client> getClient(Integer personId) {
 
         try {
             Set<Client> clients = restClient.get()
                     .uri(baseURL + clientURL + "/clients" + "?personId=" + personId)
+                    .header(X_API_KEY, adminKey)
                     .accept(APPLICATION_JSON)
                     .retrieve()
                     .body(new ParameterizedTypeReference<Set<Client>>() {
@@ -49,6 +54,7 @@ public class PanicNotificationClient {
         try {
             Set<Notification> notifications = restClient.get()
                     .uri(baseURL + "/v1/notification" + "?clientId=" + clientId)
+                    .header(X_API_KEY, adminKey)
                     .accept(APPLICATION_JSON)
                     .retrieve()
                     .body(new ParameterizedTypeReference<Set<Notification>>() {

eae82336cacc77697a872ed92f41809661763816
Lulwah Almisfer <con-lalmasfer@pif.gov.sa>
2024-10-18 15:43:58 +0300

    Panic Notification Service: add new migration

---
+++ Panic-Notification-Service/src/main/resources/db/migration/V6__insert_notification_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO notification (id, value) VALUES ('1', 'DISEASE');
+INSERT INTO notification (id, value) VALUES ('2', 'SUSPICIOUS');
+INSERT INTO notification (id, value) VALUES ('3', 'BLACKLISTED');
+
+
